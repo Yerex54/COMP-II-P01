b2ba77c02515010c4208e9aaedb286d0cd550e2f
Yerex54 <joy808148@gmail.com>
2026-04-19 23:38:25 +0100

    Block created

--- Board.py
+++ Board.py
@@ -2,25 +2,16 @@ class Board:
     def __init__(self, board):
         self.board = board
 
-    def block(self):
-        row = len(self.board)
-        for i in range(row):
-            col = len(self.board[i])
-            for j in range(col):
-                if self.board[i][j] == 2:
-                    print(f"Block at position ({i},{j})")
+    def check_tile(self, value):
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == value:
+                    return (i, j)
+        return None
 
-    def goal(self):
-        row = len(self.board)
-        for i in range(row):
-            col = len(self.board[i])
-            for j in range(col):
-                if self.board[i][j] == 9:
-                    print(f"Goal at position ({i},{j})")
-
-    def empty_cell(self, i, j):
+    def valid_cell(self, i, j):
         if 0 <= i < len(self.board) and 0 <= j < len(self.board[i]):
-            return self.board[i][j] == 0
+            return self.board[i][j] != 0
         return False
     def display(self):
         for row in self.board:
@@ -35,7 +26,8 @@ b1 = Board([
     [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
 ])
 
-b1.block()
-b1.goal()
-print(b1.empty_cell(1,2))
-b1.display()
+if __name__ == "__main__":
+    b1.check_tile(2)
+    b1.check_tile(9)
+    print(b1.valid_cell(1, 2))
+    b1.display()
