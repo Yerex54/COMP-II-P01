7ad7437cd89a7450d8fbde9a52143f407775f390
Yerex54 <joy808148@gmail.com>
2026-04-19 14:13:10 +0100

    Created game Board

--- Board.py
+++ Board.py
@@ -1,11 +1,41 @@
-Board = [
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def block(self):
+        row = len(self.board)
+        for i in range(row):
+            col = len(self.board[i])
+            for j in range(col):
+                if self.board[i][j] == 2:
+                    print(f"Block at position ({i},{j})")
+
+    def goal(self):
+        row = len(self.board)
+        for i in range(row):
+            col = len(self.board[i])
+            for j in range(col):
+                if self.board[i][j] == 9:
+                    print(f"Goal at position ({i},{j})")
+
+    def empty_cell(self, i, j):
+        if 0 <= i < len(self.board) and 0 <= j < len(self.board[i]):
+            return self.board[i][j] == 0
+        return False
+    def display(self):
+        for row in self.board:
+            print(*row)
+
+b1 = Board([
     [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
     [1, 2, 1, 1, 1, 1, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
     [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 1, 1, 9, 1, 1],
     [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-]
+])
 
-for row in Board:
-    print(row)
+b1.block()
+b1.goal()
+print(b1.empty_cell(1,2))
+b1.display()
